9d43cd79e28e08abcf662f7d07e2a3afd0b3bcf6
Cameron Kleiman <camtheman256@gmail.com>
2022-11-16 14:31:21 -0500

    add popup to map

--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,9 +1,16 @@
-import Map from "react-map-gl";
-import 'mapbox-gl/dist/mapbox-gl.css'
+import Map, { Popup } from "react-map-gl";
+import "mapbox-gl/dist/mapbox-gl.css";
+import { useState } from "react";
 
 export default function MapPage() {
   const accessToken =
     "pk.eyJ1IjoiY2tsZWltYW4xNSIsImEiOiJjazhseTJvcnMwOWdvM2hvOHE2ejk0ZHhoIn0.dp27-MIUTPYjRr-VLw1Rqg";
+  const [popup, setPopup] = useState(null);
+
+  function handleMapClick(e) {
+    setPopup(e.lngLat);
+    // query features here for metrics
+  }
 
   return (
     <>
@@ -15,8 +22,15 @@ export default function MapPage() {
           latitude: 39.948699,
           zoom: 11,
         }}
-        style={{borderRadius: 10, height: 'calc(100vh - 200px)'}}
-      />
+        style={{ borderRadius: 10, height: "calc(100vh - 200px)" }}
+        onClick={handleMapClick}
+      >
+        {popup && (
+          <Popup longitude={popup.lng} latitude={popup.lat} closeOnClick={false} onClose={() => setPopup()}>
+            <p>Create a Project at ({popup.lng.toFixed(5)}, {popup.lat.toFixed(5)})</p>
+          </Popup>
+        )}
+      </Map>
     </>
   );
 }
