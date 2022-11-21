0bb44edcf6fcc1ce9c729776cde59913772af79b
bethtes <btes95@mit.edu>
2022-11-20 22:57:34 -0500

    Update Submission.js

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -25,17 +25,14 @@ export default function SubmissionPage() {
       <Button icon={<UploadOutlined />}>Upload</Button>
     </Upload>
   </Space>
-
+<br></br>
       <form action="/my-handling-form-page" method="post">
   <ul>
     <li>
       <label for="name">Name:</label>
       <input type="text" id="name" name="user_name" />
     </li>
-    <li>
-      <label for="mail">E-mail:</label>
-      <input type="email" id="mail" name="user_email" />
-    </li>
+    <br></br>
     <li>
       <label for="msg">Message:</label>
       <textarea id="msg" name="user_message"></textarea>
