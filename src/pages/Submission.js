acd1c7252cf4dff8e7a1c7f89f8491e5dcba84a2
bethtes <btes95@mit.edu>
2022-11-20 22:31:24 -0500

    Update Submission.js

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -29,12 +29,33 @@ export default function SubmissionPage() {
       <textarea id="msg" name="user_message"></textarea>
     </li>
   </ul>
-  
-  <form action="/action_page.php">
-  <input type="file" id="myFile" name="filename">
-  <input type="submit">
-</form>
 
+  import { UploadOutlined } from '@ant-design/icons';
+import { Button, Space, Upload } from 'antd';
+import React from 'react';
+
+const App: React.FC = () => (
+  <Space direction="vertical" style={{ width: '100%' }} size="large">
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      listType="picture"
+      maxCount={1}
+    >
+      <Button icon={<UploadOutlined />}>Upload (Max: 1)</Button>
+    </Upload>
+    <Upload
+      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+      listType="picture"
+      maxCount={3}
+      multiple
+    >
+      <Button icon={<UploadOutlined />}>Upload (Max: 3)</Button>
+    </Upload>
+  </Space>
+);
+
+export default App;
+  
 </form>
 
     </Layout.Content>
