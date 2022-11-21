07494b436fa398a66bb79e67efdb3e5b717f2868
Cameron Kleiman <camtheman256@gmail.com>
2022-11-20 21:06:51 -0500

    fix imports

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -1,19 +1,18 @@
 import { Layout } from "antd";
-import { Content, Header } from "antd/lib/layout/layout";
 import { useParams } from "react-router-dom";
 import logo from '../assets/images/cogrow-logo.png';
 
 export default function SubmissionPage() {
   const { project } = useParams();
   return <Layout className="layout-dashboard">
-    <Header>
+    <Layout.Header>
       <div className="brand">
         <img src={logo} alt="" />
         <span>CoGrow</span>
       </div>
-    </Header>
-    <Content>
+    </Layout.Header>
+    <Layout.Content>
       <h1>Submit a Design for Project {project}</h1>
-    </Content>
+    </Layout.Content>
   </Layout>
 }
