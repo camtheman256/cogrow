8f0c5f9b1c983c9a76829f69722bd439aacaf9b8
Cameron Kleiman <camtheman256@gmail.com>
2022-11-20 22:13:11 -0500

    format submission page

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -1,37 +1,35 @@
 import { Layout } from "antd";
 import { useParams } from "react-router-dom";
-import logo from '../assets/images/cogrow-logo.png';
+import logo from "../assets/images/cogrow-logo.png";
 
 export default function SubmissionPage() {
   const { project } = useParams();
-  return <Layout className="layout-dashboard">
-    <Layout.Header>
-      <div className="brand">
-        <img src={logo} alt="" />
-        <span>CoGrow</span>
-      </div>
-    </Layout.Header>
-    <Layout.Content>
-      <h1>Submit a Design for Project {project}</h1>
-      
-      <form action="/my-handling-form-page" method="post">
-  <ul>
-    <li>
-      <label for="name">Name:</label>
-      <input type="text" id="name" name="user_name" />
-    </li>
-    <li>
-      <label for="mail">E-mail:</label>
-      <input type="email" id="mail" name="user_email" />
-    </li>
-    <li>
-      <label for="msg">Message:</label>
-      <textarea id="msg" name="user_message"></textarea>
-    </li>
-  </ul>
-</form>
+  return (
+    <Layout className="layout-dashboard">
+      <Layout.Header>
+        <div className="brand">
+          <img src={logo} alt="" />
+          <span>CoGrow</span>
+        </div>
+      </Layout.Header>
+      <Layout.Content>
+        <h1>Submit a Design for Project {project}</h1>
 
-    </Layout.Content>
-  </Layout>
-  }
-  </Layout>
+        <form>
+            <div>
+              <label for="name">Name:</label>
+              <input type="text" id="name" name="user_name" />
+            </div>
+            <div>
+              <label for="mail">E-mail:</label>
+              <input type="email" id="mail" name="user_email" />
+            </div>
+            <div>
+              <label for="msg">Message:</label>
+              <textarea id="msg" name="user_message"></textarea>
+            </div>
+        </form>
+      </Layout.Content>
+    </Layout>
+  );
+}
