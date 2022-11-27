4f4c2a3a112e0488c7d6b322d8916d60f493a7d7
Cameron Kleiman <camtheman256@gmail.com>
2022-11-26 20:07:55 -0600

    form spacing

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -1,75 +1,63 @@
 import { Form, Input, Layout } from "antd";
 import { useParams } from "react-router-dom";
 import logo from "../assets/images/cogrow-logo.png";
-import { UploadOutlined } from '@ant-design/icons';
-import { Button, Space, Upload } from 'antd';
+import { UploadOutlined } from "@ant-design/icons";
+import { Button, Space, Upload } from "antd";
 
 export default function SubmissionPage() {
   const { project } = useParams();
-  return <Layout className="layout-dashboard">
-    <Layout.Header>
-      <div className="brand">
-        <img src={logo} alt="" />
-        <span>CoGrow</span>
-      </div>
-    </Layout.Header>
-    <Layout.Content>
-      <h1>Submit a Design for Project</h1>
+  return (
+    <Layout className="layout-dashboard">
+      <Layout.Header>
+        <div className="brand">
+          <img src={logo} alt="" />
+          <span>CoGrow</span>
+        </div>
+      </Layout.Header>
+      <Layout.Content>
+        <h1>Submit a Design for Project</h1>
 
+        <form action="/my-handling-form-page" method="post">
+          <br></br>
+          <Form.Item
+            className="username"
+            name="Name"
+            rules={[
+              {
+                required: true,
+                message: "Please input your Name!",
+              },
+            ]}
+          >
+            <Input placeholder="Name" type="Name" />
+          </Form.Item>
 
-
-   <form action="/my-handling-form-page" method="post">
-  <br></br>
-              <Form.Item
-                  className="username"
-                  name="Name"
-                  rules={[
-                    {
-                      required: true,
-                      message: "Please input your Name!",
-                    },
-                  ]}
-                >
-                  <Input placeholder="Name" type="Name" />
-                </Form.Item>
-              
-    
-                 <Input.TextArea
-                showCount
-                maxLength={100}
-                style={{
+          <Form.Item name="Message">
+            <Input.TextArea
+              style={{
                 height: 100,
-               }}
-               placeholder="Leave a Message"
-                />
+              }}
+              placeholder="Add a description"
+            />
+          </Form.Item>
 
-    <Space direction="vertical" style={{ width: '100%' }} size="large">
-    <Upload
-      action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-      listType="picture"
-      maxCount={1}
-    >
-      <Button icon={<UploadOutlined />}>Upload</Button>
-    </Upload>
-  </Space>
+            <Form.Item name="Image">
+            <Upload
+              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+              listType="picture"
+              maxCount={1}
+            >
+              <Button icon={<UploadOutlined />}>Upload</Button>
+            </Upload>
+            </Form.Item>
 
-  <br></br>
-  <br></br>
-  <br></br>
-  <Form.Item>
-                  <Button
-                    type="primary"
-                    htmlType="submit"
-                    style={{ width: "10" }}
-                  >
-                    Submit
-                  </Button>
-                </Form.Item>
-
-  
-  </form>
-
-    </Layout.Content>
-  </Layout>
-  }
-  
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: "10" }}>
+              Submit
+            </Button>
+          </Form.Item>
+        </form>
+      </Layout.Content>
+    </Layout>
+  );
+}
