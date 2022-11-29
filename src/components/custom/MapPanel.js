7762c0476bde189ca6ad884bc868cff3027ba739
Cameron Kleiman <camtheman256@gmail.com>
2022-11-29 13:34:12 -0500

    add metrics info to card

--- src/components/custom/MapPanel.js
+++ src/components/custom/MapPanel.js
@@ -1,9 +1,21 @@
-import { Card } from "antd";
+import { Card, List, Statistic, Typography } from "antd";
 
-
-export default function MapPanel({ data }) {
-
-  return <Card className="map-panel" title={data ? "About this lot" : "Pick a lot on the map"}>
-    
-  </Card>
+export default function MapPanel({ data, metrics }) {
+  return (
+    <Card
+      className="map-panel"
+      title={data ? "About this lot" : "Pick a lot on the map"}
+    >
+      {data && (
+        <>
+          <Typography.Title level={3}>
+            {data.properties.ADDRESS}
+          </Typography.Title>
+          <Statistic title="% Impervious Cover (ground and structures)" value={data.properties.PERC_IMP} precision={2} suffix="%" />
+          <Statistic title="Ownership" value={data.properties.PUBLIC_PRI} />
+          <Statistic title="Block Group Equity Index" value={metrics.properties.INDEX_} />
+        </>
+      )}
+    </Card>
+  );
 }
--- src/pages/Map.js
+++ src/pages/Map.js
@@ -1,4 +1,9 @@
-import Map, { Marker, NavigationControl, Popup, useControl } from "react-map-gl";
+import Map, {
+  Marker,
+  NavigationControl,
+  Popup,
+  useControl,
+} from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import "@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css";
 import { useCallback, useEffect, useRef, useState } from "react";
@@ -12,16 +17,18 @@ export default function MapPage() {
     "pk.eyJ1IjoibGluaC10cmluaCIsImEiOiJjbDl3dThkYWswNDNiM25wajhrZXlneTE1In0.9GCUDxTX5FwtVbHDtXz5ew";
   const [selectionMarker, setSelectionMarker] = useState();
   const [feature, setFeature] = useState();
+  const [metrics, setMetrics] = useState();
   const user = useUser();
   const map = useRef();
 
-  const onMapLoad = useCallback(() => {}, []);
+  const onMapLoad = useCallback(() => {
+  }, []);
 
   function onMapClick(e) {
-    if(e.features.length) {
-      setSelectionMarker(e.lngLat)
-      setFeature(e.features[0])
-      map.current.flyTo({center: e.lngLat, zoom: 18});
+    if (e.features.some((f) => f.layer.id === "Parcel_Ownership")) {
+      e.features.map((f) => f.layer.id === "Parcel_Ownership" ? setFeature(f) : setMetrics(f))
+      setSelectionMarker(e.lngLat);
+      map.current.flyTo({ center: e.lngLat, zoom: 18 });
     }
   }
 
@@ -36,14 +43,20 @@ export default function MapPage() {
       }}
       ref={map}
       style={{ borderRadius: 10, height: "calc(100vh - 200px)" }}
-      interactiveLayerIds={["Parcel_Ownership"]}
+      interactiveLayerIds={["Parcel_Ownership", "Block_Index_Composite"]}
       onLoad={onMapLoad}
       onClick={onMapClick}
     >
       <Geocoder accessToken={accessToken} mapboxgl={mapboxgl} />
       <NavigationControl />
-      <MapPanel data={feature} />
-      {selectionMarker && <Marker longitude={selectionMarker.lng} latitude={selectionMarker.lat} color="#090" />}
+      <MapPanel data={feature} metrics={metrics} />
+      {selectionMarker && (
+        <Marker
+          longitude={selectionMarker.lng}
+          latitude={selectionMarker.lat}
+          color="#090"
+        />
+      )}
     </Map>
   );
 }
