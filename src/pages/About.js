1c1dc1a62abad302cd0f6b573c5514053959061a
bethtes <btes95@mit.edu>
2022-11-29 18:20:44 -0500

    commit

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,18 @@
 
 export default function About() {
   return <>
-    <div className="layout-content">
+    <div className="layout-content;"
+    style={{margin: '40px'}}>
     </div>
+    
+    <center>
     <h1>
     CoGrow is an on-the-ground, crowd-sourced visualization tool used to gain insights into the community’s vision and promote the facilitation of health and prosperity in Philadelphia’s most inequitable neighborhoods.
      The CoGrow platform combines data science, GIS mapping, and traditional community engagement to ensure that each resident has access to information about the key issues of their neighborhood and how they can be improved through green infrastructure while empowering residents to take part in its revisioning process. 
     </h1>
     <br></br>
-    <br></br>
     <h1>CoGrow invites Philadelphians to design their own green future.</h1>
-    
+    </center>
+    <br></br>
   </>
 }
