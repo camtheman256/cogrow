8fbf361b43a1bf2398873221016451ed546b8450
Cameron Kleiman <camtheman256@gmail.com>
2022-11-29 22:20:26 -0500

    implement design uploading

--- src/pages/Submission.js
+++ src/pages/Submission.js
@@ -1,11 +1,46 @@
-import { Form, Input, Layout } from "antd";
-import { useParams } from "react-router-dom";
+import { Form, Input, Layout, message } from "antd";
+import { useHistory, useParams } from "react-router-dom";
 import logo from "../assets/images/cogrow-logo.png";
 import { UploadOutlined } from "@ant-design/icons";
 import { Button, Space, Upload } from "antd";
+import { useEffect, useState } from "react";
+import { addDoc, collection, doc, getDoc } from "firebase/firestore";
+import { db, storage } from "../firebase";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 
 export default function SubmissionPage() {
   const { project } = useParams();
+  const history = useHistory();
+  const projectRef = doc(db, "projects", project);
+  const [projectData, setProjectData] = useState();
+  const [files, setFiles] = useState([]);
+
+  useEffect(() => {
+    getDoc(projectRef).then((doc) => {
+      if (doc.exists()) {
+        setProjectData(doc.data());
+      } else {
+        history.push("/");
+      }
+    });
+  }, [projectRef, history]);
+
+  async function handleSubmit(values) {
+    if (!files.length) {
+      message.error("Need to upload a design to submit.");
+      return;
+    }
+    const file = files[0];
+    const uploadRef = ref(storage, `${project}/${file.name}`);
+    const upload = await uploadBytes(uploadRef, file.originFileObj);
+    const submissions = collection(db, "projects", project, "submissions");
+    const result = await addDoc(submissions, {
+      ...values,
+      picture: await getDownloadURL(uploadRef),
+    });
+    message.success("Design uploaded successfully!");
+  }
+
   return (
     <Layout className="layout-dashboard">
       <Layout.Header>
@@ -14,50 +49,56 @@ export default function SubmissionPage() {
           <span>CoGrow</span>
         </div>
       </Layout.Header>
-      <Layout.Content>
-        <h1>Submit a Design for Project</h1>
+      {projectData ? (
+        <Layout.Content>
+          <h1>Submit a Design for Project {projectData.data.ADDRESS}</h1>
 
-        <form action="/my-handling-form-page" method="post">
-          <br></br>
-          <Form.Item
-            className="username"
-            name="Name"
-            rules={[
-              {
-                required: true,
-                message: "Please input your Name!",
-              },
-            ]}
-          >
-            <Input placeholder="Name" type="Name" />
-          </Form.Item>
-
-          <Form.Item name="Message">
-            <Input.TextArea
-              style={{
-                height: 100,
-              }}
-              placeholder="Add a description"
-            />
-          </Form.Item>
-
-            <Form.Item name="Image">
-            <Upload
-              action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
-              listType="picture"
-              maxCount={1}
+          <Form onFinish={handleSubmit}>
+            <br></br>
+            <Form.Item
+              className="username"
+              name="name"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your Name!",
+                },
+              ]}
             >
-              <Button icon={<UploadOutlined />}>Upload</Button>
-            </Upload>
+              <Input placeholder="Name" type="Name" />
             </Form.Item>
 
-          <Form.Item>
-            <Button type="primary" htmlType="submit" style={{ width: "10" }}>
-              Submit
-            </Button>
-          </Form.Item>
-        </form>
-      </Layout.Content>
+            <Form.Item name="description">
+              <Input.TextArea
+                style={{
+                  height: 100,
+                }}
+                placeholder="Add a description"
+              />
+            </Form.Item>
+
+            <Form.Item>
+              <Upload
+                listType={"picture"}
+                maxCount={1}
+                accept="image/*"
+                onChange={({ fileList }) => setFiles(fileList)}
+                beforeUpload={() => false}
+              >
+                <Button icon={<UploadOutlined />}>Upload your design</Button>
+              </Upload>
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" style={{ width: "10" }}>
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        </Layout.Content>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </Layout>
   );
 }
