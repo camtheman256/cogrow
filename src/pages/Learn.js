aeb29da814e2c7878c3f80ce7b3f032577dc817f
bethtes <btes95@mit.edu>
2022-12-14 07:25:58 -0500

    Update Learn.js

--- src/pages/Learn.js
+++ src/pages/Learn.js
@@ -1,73 +1,93 @@
 import learnbg from "../assets/images/GSI_PWD.jpeg";
+import {  Col, Col as div, Row, Typography } from "antd";
 export default function Learn() {
   return <>
 
+     <div className="learn-img"
+              style={{ padding: 12 }}
+              xs={{ span: 24 }}
+              lg={{ span: 12 }}
+              md={{ span: 12 }}></div>
+              <img src={learnbg} alt="" />
 
    <div className="layout-content"> 
    <br></br>
-    <h1>What is Green Infrastructure?</h1>
+   <Typography.Title level={1}>What is Green Infrastructure?</Typography.Title>
     <br></br>
+    <Typography.Title level={4}>
     “Green stormwater infrastructure (GSI) is an approach to managing stormwater runoff in ways that mimic the natural environment as much as possible, using plants, soil, and stone to filter and manage stormwater more effectively, reducing how much enters our sewer systems, and protecting our rivers and streams.”
+    <br></br>
     <br></br> - Philadelphia Water Department
+    </Typography.Title>
     <br></br>
 
   
 <br></br>
 <br></br>
 
-      <h1>Permeable paving</h1>
-      Permeable or porous pavement allows water to soak through the surface rather than flow over the surface and into storm drains. This system provides the structural support of conventional pavement but also has an underground stone reservoir.
+<Typography.Title level={5}>Permeable paving</Typography.Title>
+<h1>Permeable or porous pavement allows water to soak through the surface rather than flow over the surface and into storm drains. This system provides the structural support of conventional pavement but also has an underground stone reservoir.
+</h1>
 <br></br>
 <br></br>
-      <h1>Rain garden</h1>
-A rain garden is vegetation designed to collect runoff from impervious surfaces, allowing water to infiltrate the ground. Rain garden plants are generally robust native species that can thrive in extremely wet and dry weather.
+<Typography.Title level={5}>Rain garden</Typography.Title>
+<h1>A rain garden is vegetation designed to collect runoff from impervious surfaces, allowing water to infiltrate the ground. Rain garden plants are generally robust native species that can thrive in extremely wet and dry weather.
+</h1>
 <br></br>
 <br></br>
-      <h1>Cisterns & Rain barrel</h1>
-A rain barrel is a storage container connected to a downspout that captures stormwater runoff from the roof. The stored water can be used to water non-edible plants or for outdoor cleaning.
+<Typography.Title level={5}>Cisterns & Rain barrel</Typography.Title>
+<h1> A rain barrel is a storage container connected to a downspout that captures stormwater runoff from the roof. The stored water can be used to water non-edible plants or for outdoor cleaning.
+</h1>
 <br></br>
 <br></br>
-      <h1>Stormwater planter</h1>
-A stormwater planter is a specialized sidewalk system that captures runoff from streets and sidewalks. The planter is lined with permeable fabric, filled with gravel or stone, and topped off with vegetation.
+<Typography.Title level={5}>Stormwater planter</Typography.Title>
+<h1> A stormwater planter is a specialized sidewalk system that captures runoff from streets and sidewalks. The planter is lined with permeable fabric, filled with gravel or stone, and topped off with vegetation.
+</h1>
 <br></br>
 <br></br>
-      <h1>Swales</h1>
-A swale is a grassy depression that controls stormwater velocity and infiltrates runoff where feasible. Swales are typically used to channel stormwater from a street or sidewalk to a rain garden or basin.
+<Typography.Title level={5}>Swales</Typography.Title>
+<h1> A swale is a grassy depression that controls stormwater velocity and infiltrates runoff where feasible. Swales are typically used to channel stormwater from a street or sidewalk to a rain garden or basin.
+</h1>
 <br></br>
 <br></br>
-      <h1>Tree trenches</h1>
-A tree trench is a system of trees that are connected by an underground infiltration structure. On the surface, it looks like a row of ordinary street tree pits, but underground there is a system to manage incoming runoff.
+<Typography.Title level={5}>Tree trenches</Typography.Title>
+<h1> A tree trench is a system of trees that are connected by an underground infiltration structure. On the surface, it looks like a row of ordinary street tree pits, but underground there is a system to manage incoming runoff.
+</h1>
 <br></br>
 <br></br>
-      <h1>Stormwater basins</h1>
-A stormwater basin is a vegetated depression designed to slow runoff, remove pollutants and store stormwater as a permanent pool that would otherwise cause flooding and erosion.
+<Typography.Title level={5}>Stormwater basins</Typography.Title>
+<h1> A stormwater basin is a vegetated depression designed to slow runoff, remove pollutants and store stormwater as a permanent pool that would otherwise cause flooding and erosion.
+</h1>
 <br></br>
 <br></br>
-      <h1>Stormwater Bumpout</h1>
-      <br></br>
-      <br></br>
-      <h1>Downspout Planters</h1>
-Connected to the roof, downspout planters allow stormwater from gutters to flow through and be absorbed by vegetation. The systems temporarily store runoff and filter sediment and pollutants as water infiltrates through the planter.
+<Typography.Title level={5}>Downspout Planters</Typography.Title>
+<h1> Connected to the roof, downspout planters allow stormwater from gutters to flow through and be absorbed by vegetation. The systems temporarily store runoff and filter sediment and pollutants as water infiltrates through the planter.
+</h1>
 <br></br>
 <br></br>
-      <h1>Green Gutters</h1>
-Green gutters are narrow, shallow landscaped strips along a street’s curb line that are designed to allow stormwater runoff from both the street and the sidewalk to flow directly into the system.
+<Typography.Title level={5}>Green Gutters</Typography.Title>
+<h1> Green gutters are narrow, shallow landscaped strips along a street’s curb line that are designed to allow stormwater runoff from both the street and the sidewalk to flow directly into the system.
+</h1>
 <br></br>
 <br></br>
-      <h1>Stormwater Trees</h1>
-Stormwater trees look like typical street trees, but these specially designed systems include a deep stone pit that holds excess water during intense rainstorms and as snow melts.
+<Typography.Title level={5}>Stormwater Trees</Typography.Title>
+<h1> Stormwater trees look like typical street trees, but these specially designed systems include a deep stone pit that holds excess water during intense rainstorms and as snow melts.
+</h1>
 <br></br>
 <br></br>
-      <h1>Drainage Wells</h1>
-A drainage well is a manhole structure that allows stormwater runoff to flow in through holes on its cover or funnel in from other sources. The system discharges the water into surrounding soil.
+<Typography.Title level={5}>Drainage Wells</Typography.Title>
+<h1> A drainage well is a manhole structure that allows stormwater runoff to flow in through holes on its cover or funnel in from other sources. The system discharges the water into surrounding soil.
+</h1>
 <br></br>
 <br></br>
-      <h1>Stormwater Planters</h1>
-A stormwater planter is a specialized sidewalk system that captures runoff from streets and sidewalks. The planter is lined with permeable fabric, filled with gravel or stone, and topped off with vegetation.
+<Typography.Title level={5}>Stormwater Planters</Typography.Title>
+<h1> A stormwater planter is a specialized sidewalk system that captures runoff from streets and sidewalks. The planter is lined with permeable fabric, filled with gravel or stone, and topped off with vegetation.
+</h1>
 <br></br>
 <br></br>
-      <h1>Subsurface Trenches</h1>
-A storage trench is a subsurface rectangular bed cut into the street or sidewalk. The storage areas are designed to infiltrate stormwater or slow its flow into the sewer system.
+<Typography.Title level={5}>Subsurface Trenches</Typography.Title>
+<h1> A storage trench is a subsurface rectangular bed cut into the street or sidewalk. The storage areas are designed to infiltrate stormwater or slow its flow into the sewer system.
+</h1>
 <br></br>
 <br></br>
 
